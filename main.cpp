539f814731ce41d8f106551145c91bd6048e6df0
linxmouse <linxmouse@gmail.com>
2022-09-21 10:56:45 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,63 @@
+﻿#include "mainwindow.h"
+
+#include <QApplication>
+#include <QDebug>
+#include <QVariant>
+#include <QVariantMap>
+#include <QFile>
+
+#include <QJson/Parser>
+#include <QJson/QObjectHelper>
+#include <QJson/Serializer>
+
+#include <person.h>
+
+using namespace QJson;
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    person p;
+    p.setName("SimpleJsonEx");
+    p.setPhoneNumber("13388888888");
+    p.setGender(person::MALE);
+    p.setDob(QDate(1970,1,1));
+
+    // 对象序列化到字符串
+    QVariantMap map = QObjectHelper::qobject2qvariant(&p);
+    Serializer serializer;
+    serializer.setIndentMode(IndentMode::IndentFull);
+    QByteArray json = serializer.serialize(map);
+    qDebug() << json;
+
+    // 修改对象值
+    map["name"] = "NameChanged";
+    map["phoneNumber"] = "18866666666";
+    json = serializer.serialize(map);
+    qDebug() << json;
+
+    // 保存Json
+    QFile fw("person.json");
+    fw.open(QIODevice::WriteOnly | QIODevice::Text);
+    fw.write(json);
+    fw.close();
+
+    // 读取Json
+    QFile fr;
+    fr.setFileName("person.json");
+    fr.open(QIODevice::ReadOnly | QIODevice::Text);
+    QByteArray raw = fr.readAll();
+    fr.close();
+
+    // 字符串(解析)反序列化到对象
+    Parser parser;
+    QVariant variant = parser.parse(raw);
+    person dp;
+    QObjectHelper::qvariant2qobject(variant.toMap(), &dp);
+    qDebug() << dp.name() << dp.phoneNumber() << dp.gender() << dp.dob();
+
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
---
+++ person.h
@@ -0,0 +1,42 @@
+﻿#ifndef PERSON_H
+#define PERSON_H
+
+#include <QObject>
+#include <QDate>
+
+class person : public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QString name READ name WRITE setName)
+    Q_PROPERTY(QString phoneNumber READ phoneNumber WRITE setPhoneNumber)
+    Q_PROPERTY(GENDER gender READ gender WRITE setGender)
+    Q_PROPERTY(QDate dob READ dob WRITE setDob)
+    Q_ENUMS(GENDER)
+public:
+    explicit person(QObject *parent = nullptr);
+    ~person() {}
+
+    QString name() const {return _name;}
+    void setName(const QString &name) {_name = name;}
+
+    QString phoneNumber() const {return _phoneNumber;}
+    void setPhoneNumber(const QString phoneNumber) {_phoneNumber = phoneNumber;}
+
+    enum GENDER {MALE, FEMALE};
+    void setGender(GENDER gender) {_gender = gender;}
+    GENDER gender() const {return _gender;}
+
+    QDate dob() const {return _dob;}
+    void setDob(const QDate &dob) {_dob = dob;}
+signals:
+
+
+private:
+    QString _name;
+    QString _phoneNumber;
+    GENDER _gender;
+    QDate _dob;
+};
+
+#endif // PERSON_H
